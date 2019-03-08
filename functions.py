89d30209b6020b8bfa51a24233948d47d7d17785
nlune <phenixlune@gmail.com>
2019-03-08 21:40:17 +0100

    added gui, working except need to fix bug generates bad imgs

--- functions.py
+++ functions.py
@@ -7,10 +7,6 @@ def extract_fn(tfrecord):
     # use with TFRecordDataset map function to get images from tfrecord
     # Extract features using the keys set during creation
     features = {
-        'filename': tf.FixedLenFeature([], tf.string),
-        'rows': tf.FixedLenFeature([], tf.int64),
-        'cols': tf.FixedLenFeature([], tf.int64),
-        'channels': tf.FixedLenFeature([], tf.int64),
         'image': tf.FixedLenFeature([], tf.string)
     }
 
@@ -18,6 +14,27 @@ def extract_fn(tfrecord):
     sample = tf.parse_single_example(tfrecord, features)
 
     image = tf.image.decode_image(sample['image'])
-    img_shape = tf.stack([sample['rows'], sample['cols'], sample['channels']])
-    filename = sample['filename']
+
     return image
+
+
+def loadpb(filename, model_name='dcgan'):
+    """Loads pretrained graph from ProtoBuf file
+    Arguments:
+        filename - path to ProtoBuf graph definition
+        model_name - prefix to assign to loaded graph node names
+    Returns:
+        graph, graph_def - as per Tensorflow definitions
+    """
+    with tf.gfile.GFile(filename, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def,
+                            input_map=None,
+                            return_elements=None,
+                            producer_op_list=None,
+                            name=model_name)
+
+    return graph, graph_def
